b6cf27c8b1c1bfc69df62bac8d939fcc66a6c378
David Berthiaume <david.berthiaume@avalara.com>
2023-04-21 23:38:08 -0400

    Added double jump and jumping animation by adjusting the width and height of the player character.

--- gamegen/gamegen.py
+++ gamegen/gamegen.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class Game(arcade.Window):
     def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+        super().__init__(width, height, title, fullscreen=True)
         arcade.set_background_color(arcade.color.BLACK)
         
         self.wall_list = None
@@ -13,6 +13,10 @@ class Game(arcade.Window):
         self.player_sprite = None        
         self.level = None
         self.camera = None
+        
+        self.key_left = False
+        self.key_right = False
+
         
 
     def setup(self):
@@ -27,7 +31,7 @@ class Game(arcade.Window):
         wall_array[:, :, :3] = 128
         wall_texture = arcade.Texture(name = "wall", image = Image.fromarray(wall_array))
                 
-        player_array = np.zeros((28, 28, 4), dtype=np.uint8)
+        player_array = np.zeros((PLAYER_WIDTH, PLAYER_HEIGHT, 4), dtype=np.uint8)
         player_array[:, :, 0] = 255
         player_array[:, :, 3] = 255        
         player_texture = arcade.Texture(name = "player", image = Image.fromarray(player_array))
@@ -49,8 +53,11 @@ class Game(arcade.Window):
         
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self.player_sprite, gravity_constant=GRAVITY, walls=self.wall_list)
+        self.physics_engine.enable_multi_jump(2)
         
         self.camera = arcade.Camera(self.width, self.height)
+        self.world_pixel_height = WORLD_HEIGHT * TILE_HEIGHT
+        self.world_pixel_width = WORLD_WIDTH * TILE_WIDTH
         
     def on_draw(self):
         self.camera.use()
@@ -63,23 +70,24 @@ class Game(arcade.Window):
         if key == arcade.key.UP or key == arcade.key.SPACE:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+                self.physics_engine.increment_jump_counter()
+      
+                
+     
+        if key == arcade.key.LEFT:
+            self.key_left = True
         elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
-        
+            self.key_right = True
                 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
+        if key == arcade.key.LEFT:
+            self.key_left = False
+        elif key == arcade.key.RIGHT:
+            self.key_right = False
+        elif key == arcade.key.ESCAPE:
+            arcade.close_window()
             
     def center_camera_to_player(self):
         screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
@@ -92,14 +100,40 @@ class Game(arcade.Window):
             screen_center_x = 0
         if screen_center_y < 0:
             screen_center_y = 0
+        # Don't let the camera travel past the right/bottom of the map.
+        if screen_center_x > self.world_pixel_width - self.camera.viewport_width:
+            screen_center_x = self.world_pixel_width - self.camera.viewport_width
+        if screen_center_y > self.world_pixel_height - self.camera.viewport_height:
+            screen_center_y = self.world_pixel_height - self.camera.viewport_height
          
         player_centered = screen_center_x, screen_center_y
 
         self.camera.move_to(player_centered)
     
     def on_update(self, delta_time):
+        
+
+        # move the player
+        x_movement = 0.0
+        if self.key_left:
+            x_movement-=PLAYER_MOVEMENT_SPEED
+        if self.key_right:
+            x_movement+=PLAYER_MOVEMENT_SPEED
+        self.player_sprite.change_x = x_movement
+        
+        if self.player_sprite.change_y > 0.0:
+            self.player_sprite.height = PLAYER_HEIGHT * 1.15
+            self.player_sprite.width = PLAYER_WIDTH * 0.9
+        else:
+            self.player_sprite.height = PLAYER_HEIGHT
+            self.player_sprite.width = PLAYER_WIDTH
+
+
+    
         self.physics_engine.update()
         self.center_camera_to_player()
+        
+    
                 
 def main():
     window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
